3f6c5c6265fffc7726d2daf8a68c05efed2026d4
Roel Van Gils <roel@elevenways.be>
2025-03-23 08:06:20 +0100

    Remove old binary files and update compile script to output binaries to the bin directory

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# First, compile for ARM64 (your native architecture)
-/opt/homebrew/bin/shc -f appearance.sh -o downloads/appearance-arm64
+# First, compile for ARM64
+/opt/homebrew/bin/shc -f appearance.sh -o bin/appearance-arm64
 
 # Then compile for Intel (x86_64)
-arch -x86_64 /usr/local/bin/shc -f appearance.sh -o downloads/appearance-x86_64
+arch -x86_64 /usr/local/bin/shc -f appearance.sh -o bin/appearance-x86_64
 
 # Create a universal binary
-lipo -create downloads/appearance-arm64 downloads/appearance-x86_64 -output downloads/appearance
+lipo -create bin/appearance-arm64 bin/appearance-x86_64 -output bin/appearance
