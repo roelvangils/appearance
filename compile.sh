da5554dfff708d3da67cb704f5076c5dc56aec95
Roel Van Gils <roel@elevenways.be>
2025-03-24 09:04:13 +0100

    Add comments to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # First, compile for ARM64
+# We used the Apple Silicon (Homebrew) version of SHC for this
 /opt/homebrew/bin/shc -f appearance.sh -o bin/appearance-arm64
 
 # Then compile for Intel (x86_64)
+# We used the Intel version of SHC for this
 arch -x86_64 /usr/local/bin/shc -f appearance.sh -o bin/appearance-x86_64
 
 # Create a universal binary
